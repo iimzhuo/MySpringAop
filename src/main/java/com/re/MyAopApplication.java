133c430fba6efc28d5f1936261d4435b8898155c
iimzhuo <1822638890@qq.com>
2021-06-30 17:59:31 +0800

    存在bug

--- src/main/java/com/re/MyAopApplication.java
+++ src/main/java/com/re/MyAopApplication.java
@@ -3,12 +3,14 @@ package com.re;
 import com.re.annotation.MyEnableAspect;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+//@SpringBootApplication
 @MyEnableAspect
+@ComponentScan("com.re")
 public class MyAopApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MyAopApplication.class, args);
+        //SpringApplication.run(MyAopApplication.class, args);
     }
 }
--- src/main/java/com/re/Tools/MyTools.java
+++ src/main/java/com/re/Tools/MyTools.java
@@ -7,11 +7,11 @@ import java.util.Map;
 public class MyTools {
     public static final String MyAspect="com.re.annotation.MyAspect";
 
-    public static final String MyBefore="";
+    public static final String MyBefore="com.re.annotation.MyBefore";
 
-    public static final String MyAfter="";
+    public static final String MyAfter="com.re.annotation.MyAfter";
 
-    public static final String MyAround="";
+    public static final String MyAround="com.re.annotation.MyAround";
 
     //定义target class和advice，之间的对应关系，入过这个类被标记为target class,那么
     public static Map<String, List<MyAspectIn>> map=new HashMap<>();
