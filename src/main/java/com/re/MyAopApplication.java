cba9f245137c27a1212247c7ed1276949488264b
iimzhuo <1822638890@qq.com>
2021-07-01 10:52:12 +0800

    第二次bug完整修复

--- src/main/java/com/re/MyAopApplication.java
+++ src/main/java/com/re/MyAopApplication.java
@@ -1,16 +1,23 @@
 package com.re;
 
 import com.re.annotation.MyEnableAspect;
+import com.re.service.WechatService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+//@SpringBootApplication
 @MyEnableAspect
-//@ComponentScan("com.re")
+@ComponentScan("com.re")
 public class MyAopApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MyAopApplication.class, args);
+        //SpringApplication.run(MyAopApplication.class, args);
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(MyAopApplication.class);
+        WechatService bean = context.getBean(WechatService.class);
+        System.out.println(bean.getClass());
+        bean.pay();
     }
 }
